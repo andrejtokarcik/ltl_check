cb4e4dd9df00daae27967c4b129a3a8c853f948f
Andrej Tokarčík <andrejtokarcik@gmail.com>
2013-12-16 14:11:04 +0100

    atoms_interpreter -> label_function

--- kripke.py
+++ kripke.py
@@ -6,10 +6,10 @@ def powerset(iterable):
                                    for r in range(len(s)+1)))
 
 class KripkeStructure(object):
-    def __init__(self, init_state, succs_function, atoms_interpreter):
+    def __init__(self, init_state, succs_function, label_function):
         self.init_state = init_state
         self.succs_function = succs_function
-        self.atoms_interpreter = atoms_interpreter
+        self.label_function = label_function
 
         # Generate states using the succs function (pretty naively).
         self.states = set()
@@ -23,14 +23,14 @@ class KripkeStructure(object):
         # Obtain the set of atomic propositions.
         self.atoms = set()
         for state in self.states:
-            self.atoms.update(atoms_interpreter(state))
+            self.atoms.update(label_function(state))
 
     def as_buchi(self):
         alphabet = powerset(self.atoms)
         def trans_function(state, symbol):
             assert state in self.states
             assert symbol in alphabet
-            if set(self.atoms_interpreter(state)) == set(symbol):
+            if set(self.label_function(state)) == set(symbol):
                 return self.succs_function(state)
 
         return BuchiAutomaton(states=self.states,
